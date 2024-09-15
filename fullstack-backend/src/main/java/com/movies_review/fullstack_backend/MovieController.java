d797ff942453ea2ab0a889cbc1332c033288070e
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-09-14 23:09:34 -0400

    Fetch a Single Movie

--- fullstack-backend/src/main/java/com/movies_review/fullstack_backend/MovieController.java
+++ fullstack-backend/src/main/java/com/movies_review/fullstack_backend/MovieController.java
@@ -1,13 +1,17 @@
 package com.movies_review.fullstack_backend;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -19,8 +23,8 @@ public class MovieController {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
-    @GetMapping('/{id}')
-    public ResponseEntity<Movie> getSingleMovie(){
-        
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id){
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id), HttpStatus.OK);
     }
 }
--- fullstack-backend/src/main/java/com/movies_review/fullstack_backend/MovieService.java
+++ fullstack-backend/src/main/java/com/movies_review/fullstack_backend/MovieService.java
@@ -1,9 +1,12 @@
 package com.movies_review.fullstack_backend;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -11,5 +14,9 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie>  singleMovie(ObjectId id){
+        return movieRepository.findById(id);
     }
 }

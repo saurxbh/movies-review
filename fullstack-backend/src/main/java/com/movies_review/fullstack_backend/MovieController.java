b866725f8b960d584eff425e30cb27e973f9bbc9
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-09-14 23:00:32 -0400

    Create Controller

---
+++ fullstack-backend/src/main/java/com/movies_review/fullstack_backend/MovieController.java
@@ -0,0 +1,26 @@
+package com.movies_review.fullstack_backend;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping('/{id}')
+    public ResponseEntity<Movie> getSingleMovie(){
+        
+    }
+}

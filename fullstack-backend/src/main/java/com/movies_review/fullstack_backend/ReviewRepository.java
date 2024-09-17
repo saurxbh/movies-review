529f6b556669b96f34ee0d0307e0a81701772bf3
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-09-17 18:31:52 -0400

    Create Review Service

---
+++ fullstack-backend/src/main/java/com/movies_review/fullstack_backend/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.movies_review.fullstack_backend;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
